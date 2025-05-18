7ccb87ce07726264575ef95cd14d82c1765a8e28
vinceclave <datanaganvince351@gmail.com>
2025-05-18 13:38:38 +0800

    revision

--- server/server.js
+++ server/server.js
@@ -12,10 +12,19 @@ app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
 app.use(cors({
-   origin: [
-      'http://localhost:4200',
-      'https://4200-firebase-user-management-system-1747543700327.cluster-sumfw3zmzzhzkx4mpvz3ogth4y.cloudworkstations.dev'
-   ],
+   origin: function(origin, callback) {
+      const allowedOrigins = [
+         'http://localhost:4200',
+         'https://4200-firebase-user-management-system-1747543700327.cluster-sumfw3zmzzhzkx4mpvz3ogth4y.cloudworkstations.dev'
+      ];
+      // Allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+         const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+         return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+   },
    credentials: true,
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
